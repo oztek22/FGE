2b0288f086b3ddf048cc4491b5177141cd855bc3
oztek22 <yashndave@gmail.com>
2019-05-20 21:10:08 +0530

    app-vertical-total-bar-chart dynamic

--- src/app/components/charts/vertical-total-bar-chart/vertical-total-bar-chart.component.ts
+++ src/app/components/charts/vertical-total-bar-chart/vertical-total-bar-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-vertical-total-bar-chart',
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vertical-total-bar-chart.component.scss']
 })
 export class VerticalTotalBarChartComponent implements OnInit {
+  @Input()
+  chartData = [
+    {
+      'percentage': 47.6,
+      'description': 'available'
+    },
+    {
+      'percentage': 52.4,
+      'description': 'obligated'
+    }
+  ]
 
   constructor() { }
 
