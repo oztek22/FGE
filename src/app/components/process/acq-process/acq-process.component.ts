23907b5dad35c3f1314d2e413969a21ed960175b
oztek22 <yashndave@gmail.com>
2019-05-20 21:03:12 +0530

    acq process dynamic

--- src/app/components/process/acq-process/acq-process.component.ts
+++ src/app/components/process/acq-process/acq-process.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-acq-process',
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./acq-process.component.scss']
 })
 export class AcqProcessComponent implements OnInit {
+  @Input()
+  timelineData = [
+    {header: 'Procurement Request', subHeader: '170 Actions'},
+    {header: 'Market Research', subHeader: '340 Actions'},
+    {header: 'ACQ Strategy Formulation', subHeader: '150 Actions'},
+    {header: 'Request for Proposal', subHeader: '420 Actions'},
+    {header: 'Proposal Evaluation', subHeader: '350 Actions'},
+    {header: 'Awarded', subHeader: '560 Actions'}
+  ];
+
+  @Input()
+  selectedIndex = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  updateSelection(index) {
+    this.selectedIndex = index;
   }
 
 }
