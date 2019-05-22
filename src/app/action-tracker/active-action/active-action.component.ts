932799ddcdaa45bc7c546e258b18dcc8a8765dd5
oztek22 <yashndave@gmail.com>
2019-05-22 11:37:54 +0530

    acq strategy

--- src/app/action-tracker/active-action/active-action.component.ts
+++ src/app/action-tracker/active-action/active-action.component.ts
@@ -16,6 +16,8 @@ export class ActiveActionComponent implements OnInit {
   timelineData;
   selectedTimeline = -1;
 
+  strategy;
+
   constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -23,23 +25,36 @@ export class ActiveActionComponent implements OnInit {
       this.id = params['id'];
       this.getFundDetails();
       this.getTimelineData();
+      this.getStrategy();
     });
   }
 
   getTimelineData() {
     this.timelineData = [
-      { header: 'Procurement Request', subHeader: '170 Actions', days: '23 days',
-        pending: [], approved: [{ name: 'T. Caputo' }, { name: 'M. Gosnell' }, { name: 'R. Wilkerson' }] },
-      { header: 'Market Research', subHeader: '340 Actions', days: '23 days',
-        pending: [], approved: [{ name: 'T. Caputo' }, { name: 'M. Gosnell' }, { name: 'R. Wilkerson' }] },
-      { header: 'ACQ Strategy Formulation', subHeader: '150 Actions', days: '23 days',
-        pending: [{ name: 'T. Caputo' }, { name: 'M. Gosnell' }], approved: [{ name: 'R. Wilkerson' }] },
-      { header: 'Request for Proposal', subHeader: '420 Actions', days: '23 days',
-        pending: [{ name: 'T. Caputo' }, { name: 'M. Gosnell' }, { name: 'R. Wilkerson' }], approved: [] },
-      { header: 'Proposal Evaluation', subHeader: '350 Actions', days: '23 days',
-        pending: [{ name: 'T. Caputo' }, { name: 'M. Gosnell' }, { name: 'R. Wilkerson' }], approved: [] },
-      { header: 'Awarded', subHeader: '560 Actions', days: '23 days',
-        pending: [{ name: 'T. Caputo' }, { name: 'M. Gosnell' }, { name: 'R. Wilkerson' }], approved: [] }
+      {
+        header: 'Procurement Request', subHeader: '170 Actions', days: '23 days',
+        pending: [], approved: [{ name: 'T. Caputo' }, { name: 'M. Gosnell' }, { name: 'R. Wilkerson' }]
+      },
+      {
+        header: 'Market Research', subHeader: '340 Actions', days: '23 days',
+        pending: [], approved: [{ name: 'T. Caputo' }, { name: 'M. Gosnell' }, { name: 'R. Wilkerson' }]
+      },
+      {
+        header: 'ACQ Strategy Formulation', subHeader: '150 Actions', days: '23 days',
+        pending: [{ name: 'T. Caputo' }, { name: 'M. Gosnell' }], approved: [{ name: 'R. Wilkerson' }]
+      },
+      {
+        header: 'Request for Proposal', subHeader: '420 Actions', days: '23 days',
+        pending: [{ name: 'T. Caputo' }, { name: 'M. Gosnell' }, { name: 'R. Wilkerson' }], approved: []
+      },
+      {
+        header: 'Proposal Evaluation', subHeader: '350 Actions', days: '23 days',
+        pending: [{ name: 'T. Caputo' }, { name: 'M. Gosnell' }, { name: 'R. Wilkerson' }], approved: []
+      },
+      {
+        header: 'Awarded', subHeader: '560 Actions', days: '23 days',
+        pending: [{ name: 'T. Caputo' }, { name: 'M. Gosnell' }, { name: 'R. Wilkerson' }], approved: []
+      }
     ];
     this.selectedTimeline = 2;
   }
@@ -47,6 +62,25 @@ export class ActiveActionComponent implements OnInit {
   getFundDetails() {
     this.funds.requestedFunds = '$4,20,00,000';
     this.funds.obligatedFund = '-';
+  }
+
+  getStrategy() {
+    this.strategy = {
+      values: {
+        contractingVehicle: 'Navy SeaPort-E NexGen',
+        fundingType: 'MILCON',
+        targetDate: '',
+        setAside: 'HUBZone',
+        certificationsRequired: ['TOP SECRET FCL', 'MILCON BONDING', 'CERTIFIED HUBZONE']
+      },
+      team: [
+        {desig: 'Contracting Officer', name: 'Marie Gosnell', response: 'Yes'},
+        {desig: 'Contracting Specialist', name: 'Tori Caputo', response: 'Yes'},
+        {desig: 'Legal', name: 'Shawna Howell', response: 'Pending'},
+        {desig: 'Program Office', name: 'John Blake', response: 'No'},
+        {desig: 'Budget Analyst', name: 'Shawna Howell', response: 'Pending'}
+      ]
+    };
   }
 
   backToTracker() {
