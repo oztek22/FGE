b746e64d348a17d537673fd7d9bb7df88ab8dea8
oztek22 <yashndave@gmail.com>
2019-05-23 16:45:15 +0530

    calander dynamic

--- src/app/create-pr/calendar-type/calendar-type.component.ts
+++ src/app/create-pr/calendar-type/calendar-type.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-calendar-type',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar-type.component.scss']
 })
 export class CalendarTypeComponent implements OnInit {
-
+  fieldData = {
+    start: '2019-05-23',
+    end: '2019-05-23',
+    duration: 0
+  }
+  @Input()
+  data;
+  @Output()
+  fieldValueChange = new EventEmitter<any>();
+  @Input()
+  get fieldValue() {
+    return this.fieldData;
+  }
+  set fieldValue(val) {
+    this.fieldValueChange.emit(this.fieldData);
+  }
   constructor() { }
 
   ngOnInit() {
+  }
+
+  updateStartDate(e) {
+    this.fieldData.start = e.target.value;
+    this.fieldData.duration = moment(this.fieldData.end).diff(moment(this.fieldData.start), 'days');
+    this.fieldValue = this.fieldData;
+  }
+
+  updateEndDate(e) {
+    this.fieldData.end = e.target.value;
+    this.fieldData.duration = moment(this.fieldData.end).diff(moment(this.fieldData.start), 'days');
+    this.fieldValue = this.fieldData;
   }
 
 }
