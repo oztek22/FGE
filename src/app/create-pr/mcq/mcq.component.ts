88edcf4fb53e6e28803340f18288553450068de5
oztek22 <yashndave@gmail.com>
2019-05-23 11:06:07 +0530

    mcq dynamic

--- src/app/create-pr/mcq/mcq.component.ts
+++ src/app/create-pr/mcq/mcq.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-mcq',
@@ -6,7 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mcq.component.scss']
 })
 export class McqComponent implements OnInit {
-
+  fieldText;
+  @Input()
+  data;
+  @Output()
+  fieldValueChange = new EventEmitter<any>();
+  @Input()
+  get fieldValue() {
+    return this.fieldText;
+  }
+  set fieldValue(val) {
+    this.fieldText = val;
+    this.fieldValueChange.emit(this.fieldText);
+  }
   constructor() { }
 
   ngOnInit() {
